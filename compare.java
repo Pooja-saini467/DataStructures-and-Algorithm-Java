678f027fd757228e8713f7d53b1c8b3f24ffb8c0
Pooja <90523453+Pooja-saini467@users.noreply.github.com>
2022-08-05 14:40:15 +0530

    Delete compare.java

--- compare.java
+++
@@ -1,9 +0,0 @@
-public class compare {
-    public static void main(String[]args) {
-        int a = 20, b=30,c;
-        c = b;
-        b = a;
-        a = c;
-        System.out.println(+a+","+b);
-    }
-}
