9ea1d72c268624f9fc35753b61634f9f83d5b436
Pooja <90523453+Pooja-saini467@users.noreply.github.com>
2022-08-06 01:14:59 +0530

    Add files via upload

---
+++ vewol.java
@@ -0,0 +1,21 @@
+//public class vewol {
+    //public static void main(String[]args){
+    //    char ch;
+     //   for(ch='a';ch<='z';ch++){
+     //       System.out.println(""+ch);
+     //   }
+    //}
+//}
+
+import java.util.Scanner;
+
+public class vewol{
+    public static void main(String[]args){
+        char ch;
+        ch = 'A';
+        while (ch<='Z'){
+            System.out.println(""+ch);
+            ch++;
+        }
+    }
+}
